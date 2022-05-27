d85280afa555aba1b8ebce4f603832f74a68889e
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-05-27 18:04:37 +0200

    Fullscreenmode = super + y

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ https://dwm.suckless.org/
 ## Patches
 
 - [gaps](https://dwm.suckless.org/patches/gaps/)
-- [fullscreen](https://dwm.suckless.org/patches/fullscreen/) (super + f)
+- [fullscreen](https://dwm.suckless.org/patches/fullscreen/) (super + y)
 - [Xresources](https://dwm.suckless.org/patches/xresources/)
--- config.h
+++ config.h
@@ -91,22 +91,27 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      fullscreen,     {0} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_y,      fullscreen,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 
