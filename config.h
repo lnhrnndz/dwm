0da2275069c2cf544d4e2c161fb1d91508d9f8ca
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-06-06 22:57:40 +0200

    Add Steam rule
    start steam as a floating window

--- config.h
+++ config.h
@@ -30,7 +30,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+    { "Steam",    NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
